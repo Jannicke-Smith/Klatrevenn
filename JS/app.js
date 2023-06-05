a972490c4cb8a4d18d7c76c15236159ff0eb9419
Jannicke-Smith <114487881+Jannicke-Smith@users.noreply.github.com>
2023-06-05 23:06:06 +0200

    Mediaquery

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+    function alertFunskjonRegistrer() {
+        alert("Takk for at du registrerte deg kjære sensor :)")
+    }
+
+    function alertFunksjonLoggInn() {
+        alert("Har du husket å registrere deg enda?")
+    }
